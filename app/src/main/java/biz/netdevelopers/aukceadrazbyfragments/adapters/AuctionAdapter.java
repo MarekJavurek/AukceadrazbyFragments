7fd81506f9f2a2f733b491681a1013921379bcc5
Jindřich Urbánek <m3up4x@gmail.com>
2014-11-23 23:18:14 +0100

    vasmajetekproviderready

--- app/src/main/java/biz/netdevelopers/aukceadrazbyfragments/adapters/AuctionAdapter.java
+++ app/src/main/java/biz/netdevelopers/aukceadrazbyfragments/adapters/AuctionAdapter.java
@@ -22,18 +22,18 @@ public class AuctionAdapter extends ArrayAdapter<AuctionObject> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // Get the data item for this position
-        AuctionObject user = getItem(position);
+        AuctionObject auction = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_auction, parent, false);
         }
 
         // Lookup view for data population
-        TextView tvName = (TextView) convertView.findViewById(R.id.auction_name);
-        TextView tvHome = (TextView) convertView.findViewById(R.id.auction_price);
+        TextView Advert_name = (TextView) convertView.findViewById(R.id.auction_name);
+        TextView Offer_id = (TextView) convertView.findViewById(R.id.auction_price);
         // Populate the data into the template view using the data object
-        tvName.setText(user.getAdvert_name());
-        tvHome.setText("ID: " + String.valueOf(user.getOffer_id()));
+        Advert_name.setText(auction.getAdvert_name());
+        Offer_id.setText("ID: " + String.valueOf(auction.getOffer_id()));
         // Return the completed view to render on screen
         return convertView;
     }
