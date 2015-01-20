06258869ecb83d921818653ebbeea1b75e11ce75
Marek Javůrek <peanee@gmail.com>
2015-01-20 23:56:27 +0100

    reklamy ;)

---
+++ app/src/main/java/biz/netdevelopers/aukceadrazbyfragments/fragments/AdFragment.java
@@ -0,0 +1,29 @@
+package biz.netdevelopers.aukceadrazbyfragments.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import biz.netdevelopers.aukceadrazbyfragments.R;
+
+public class AdFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ad, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle bundle) {
+        super.onActivityCreated(bundle);
+        AdView mAdView = (AdView) getView().findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
